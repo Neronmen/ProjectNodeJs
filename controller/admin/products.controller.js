50ba08a718e3900e54f8403cfac70a61c896b004
Neronmen <pnkvlog1508@gmail.com>
2024-08-07 23:15:30 +0700

    Làm chức năng bộ lọc hoạt động sản phẩm

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -1,18 +1,54 @@
-    // [GET] /admin/products
-    const Product = require("../../models/product.model");
+// [GET] /admin/products
+const Product = require("../../models/product.model");
 module.exports.index = async (req, res) => {
-    const listProducts = await Product.find(
-        {
-            deleted: false
-        }
-    );
-    console.log(listProducts)
 
-    res.render("admin/pages/product/index.pug",
-        {
-            pageTitle: "Danh sách sản phẩm ",
-            listProducts: listProducts
-        }
-    )
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: ""
+
+
+    },
+    {
+      name: "Hoạt Động ",
+      status: "active",
+      class: ""
+    },
+    {
+      name: "Dừng hoạt động ",
+      status: "inactive",
+      class: ""
+    }
+    
+  ];
+
+      
+      if(req.query.status){
+        const index = filterStatus.findIndex(item => item.status == req.query.status);
+        filterStatus[index].class = "active";
+      }else{
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active";
+      }
+   
+
+
+
+
+
+    console.log(req.query.status)
+  let find = {
+    deleted: false,
+  };
+  if(req.query.status){
+    find.status = req.query.status
   }
+  const listProducts = await Product.find(find);
 
+  res.render("admin/pages/product/index.pug", {
+    pageTitle: "Danh sách sản phẩm ",
+    listProducts: listProducts,
+    filterStatus: filterStatus
+  });
+};
